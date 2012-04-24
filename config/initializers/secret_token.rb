5afe4aa85f54904b0897c2f01a074ded942508bd
Romain Gauthier <romain.gauthier@af83.com>
2012-04-24 13:02:03 +0200

    Initial revision for the MDD project.
    MDD = Ministère du Dévelopement Durable.
    
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdd::Application.config.secret_token = '057ce4440e05d32de1a8afa8ee7f532919ad572d2334807e899fb79136f365ce8e54605730abcea88f272f52c42d59cbd5c2d1f6ff3ebb8cd2c3a4662583c8de'
