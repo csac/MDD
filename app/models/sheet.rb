dfdd70769ea43a5712645126d0407f7bc7f6a935
Romain Gauthier <romain.gauthier@af83.com>
2012-04-27 17:16:45 +0200

    Sheets: render markdown data.
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -3,4 +3,9 @@ class Sheet < ActiveRecord::Base
   accepts_nested_attributes_for :keywords
 
   attr_accessible :title, :description, :level, :up_to_date, :keyword_ids
+
+  def description_html
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
 end
