22e2dbf856c73ff67603e0caff9fbd4bfd955ab3
Romain Gauthier <romain.gauthier@af83.com>
2012-05-02 14:26:29 +0200

    Sheets: mandatory fields.
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -3,10 +3,21 @@ class Sheet < ActiveRecord::Base
   has_and_belongs_to_many :keywords
   accepts_nested_attributes_for :keywords
 
+  validates :title,       :presence => true
+  validates :description, :presence => true
+  validates :level,       :presence => true
+  validate  :at_least_one_keyword
+
   attr_accessible :title, :description, :level, :up_to_date, :keyword_ids
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
+  private
+
+  def at_least_one_keyword
+    errors.add(:keywords, "There must have at least one keyword") if self.keywords.size == 0
   end
 
 end
