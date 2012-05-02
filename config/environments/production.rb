00fb2627075a87acaa88c8da4d28c59a193a6cb5
Laurent Arnoud <laurent@spkdev.net>
2012-05-02 11:22:05 +0200

    Add MY_DOMAIN for devise mailer_sender.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Mdd::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  MY_DOMAIN = 'developpement-durable.gouv.fr'
 
   # Code is not reloaded between requests
   config.cache_classes = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,6 @@
 Mdd::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  MY_DOMAIN = 'localhost:3000'
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
