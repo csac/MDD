86f246788c3537fb6a6441e3e623e12b4bf8b3d4
Romain Gauthier <romain.gauthier@af83.com>
2012-05-02 14:38:01 +0200

    Keyword: mandatory fields.
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,5 +2,9 @@
 class Keyword < ActiveRecord::Base
   belongs_to :keyword_category
   has_and_belongs_to_many :sheets
+
+  validates :name,             :presence => true
+  validates :keyword_category, :presence => true
+
   attr_accessible :name, :keyword_category, :keyword_category_id
 end
