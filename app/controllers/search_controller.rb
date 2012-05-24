81d00f8639de8afc7303273c88a731e9dd77f6c4
Romain Gauthier <romain.gauthier@af83.com>
2012-05-24 18:23:26 +0200

    Sheet: improve the UI for filters.
    Signed-off-by: Romain Gauthier <romain.gauthier@af83.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,20 @@ class SearchController < ApplicationController
   before_filter :authenticate_user!
   respond_to :html
 
+  helper_method :is_active?
+
   def index
     @sheets = Sheet.search(params).perform.results
+  end
+
+  def is_active?(filter, value)
+    if value == 'all' && params[filter].blank?
+      'active'
+    elsif params[filter] == value
+      'active'
+    else
+      ''
+    end
   end
 end
 
